54ef81fc8f599a2bb7dcef8ee440fb40e31d5a24
donMetto <manuorlando02@gmail.com>
2024-01-22 11:51:16 +0100

    cancellazione elementi durante la creazione di ex vero/falso

--- resources/views/exercises/createTfEx.blade.php
+++ resources/views/exercises/createTfEx.blade.php
@@ -10,11 +10,11 @@
                 @csrf
                 <input type="hidden" id="questionNum" name="questionNum" value="1">
                 <div id="tf-FormDiv">
-                    <div>
-                        <label for="question0">Question 1: </label>
+                    <div id="questDiv0">
+                        <label for="question0" id = "questLab0">Question 1: </label>
                         <input type="text" id="question0" name="question0" required>
                         <input type="checkbox" id="isTrue0" name="isTrue0" value="1">
-                        <label for="isTrue"> True?</label>
+                        <label for="isTrue" id="isTrueLab0"> True?</label>
                     </div>
                 </div>
             </form>
@@ -33,8 +33,12 @@
             const questionInput = document.createElement("input");
             const questionCheckbox = document.createElement("input");
             const checkboxLabel = document.createElement("label");
+            const deleteButtn = document.createElement("button")
             let n = parseInt(questionNum.value);
 
+            questionDiv.id = "questDiv" + n;
+
+            questionLabel.id = "questLab" + n;
             questionLabel.for = "question" + n;
             questionLabel.innerText = "Question "+ (1 + n) + ": ";
             questionDiv.appendChild(questionLabel);
@@ -50,12 +54,55 @@
             questionCheckbox.value = "1";
             questionDiv.appendChild(questionCheckbox);
 
+            checkboxLabel.id = "isTrueLab" + n;
             checkboxLabel.for = "isTrue" + n;
             checkboxLabel.innerText = " True?";
             questionDiv.appendChild(checkboxLabel);
 
+            deleteButtn.id = "delButt" + n;
+            deleteButtn.onclick = () => { deleteQuestion(questionDiv.id, n); };
+            deleteButtn.innerText = " Delete";
+            questionDiv.appendChild(deleteButtn);
+
             form.appendChild(questionDiv);
             questionNum.value++;
+        }
+
+        function deleteQuestion(divId, p){ // p := posizione elemento
+            const divToDelete = document.getElementById(divId);
+            let n = parseInt(questionNum.value);
+
+            divToDelete.remove();
+            if(p != n-1){
+                for(let i = p+1; i < n; i++){
+                    let questionDiv       = document.getElementById("questDiv" + i);
+                    let questionLabel     = document.getElementById("questLab" + i);
+                    let questionInput     = document.getElementById("question" + i);
+                    let questionCheckbox  = document.getElementById("isTrue" + i);
+                    let checkboxLabel     = document.getElementById("isTrueLab" + i);
+                    let deleteButtn       = document.getElementById("delButt" + i);
+
+                    questionDiv.id = "questDiv" + (i - 1);
+
+                    questionLabel.id = "questLab" + (i - 1);
+                    questionLabel.for = "question" + (i - 1);
+                    questionLabel.innerText = "Question "+ (i) + ": ";
+
+                    questionInput.id = "question" + (i - 1);
+                    questionInput.name = "question" + (i - 1);
+
+                    questionCheckbox.id = "isTrue" + (i - 1);
+                    questionCheckbox.name = "isTrue" + (i - 1);
+
+                    checkboxLabel.id = "isTrueLab" + (i - 1);
+                    checkboxLabel.for = "isTrue" + (i - 1);
+
+                    deleteButtn.id = "delButt" + (i - 1);
+                    deleteButtn.onclick = () => { deleteQuestion(questionDiv.id, i-1); };
+                }
+            }
+            questionNum.value--;
+            console.log(questionNum.value);
         }
     </script>
 </x-app-layout>
