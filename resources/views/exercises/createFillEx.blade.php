cf00f8733833f681d1fde80ff7da29dfae1722b3
donMetto <manuorlando02@gmail.com>
2024-01-22 13:03:32 +0100

    eliminazione elementi fill-in durante creazione

--- resources/views/exercises/createFillEx.blade.php
+++ resources/views/exercises/createFillEx.blade.php
@@ -24,46 +24,89 @@
         const form = document.getElementById("fill-FormDiv");
 
         function addTextElem(){
-            const elemDiv = document.createElement("div");
-            const elem = document.createElement("textarea");
-            const elemType = document.createElement("input");
+            const elemDiv     = document.createElement("div");
+            const elem        = document.createElement("textarea");
+            const elemType    = document.createElement("input");
+            const deleteButtn = document.createElement("button");
             let n = parseInt(elemNum.value);
+
+            elemDiv.id = "elemDiv" + n;
 
             elemType.type = "hidden";
             elemType.value = "text";
-            elemType.id = "elemType" + n;
-            elemType.name = "elemType" + n;
+            elemType.id = "elemType" + n;      //
+            elemType.name = "elemType" + n;    //
 
-            elem.id = "element" + n;
-            elem.name = "element" + n;
+            elem.id = "element" + n;           // x
+            elem.name = "element" + n;         // x
             elem.required = true;
             elemDiv.appendChild(elem);
             elemDiv.appendChild(elemType);
+
+            deleteButtn.id = "delButt" + n;
+            deleteButtn.onclick = () => { deleteElement(elemDiv.id, n); };
+            deleteButtn.innerText = " Delete";
+            elemDiv.appendChild(deleteButtn);
 
             form.appendChild(elemDiv);
             elemNum.value++;
         }
 
         function addInputElem(){
-            const elemDiv = document.createElement("div");
-            const elem = document.createElement("input");
-            const elemType = document.createElement("input");
+            const elemDiv     = document.createElement("div");
+            const elem        = document.createElement("input");
+            const elemType    = document.createElement("input");
+            const deleteButtn = document.createElement("button");
             let n = parseInt(elemNum.value);
+
+            elemDiv.id = "elemDiv" + n;
 
             elemType.type = "hidden";
             elemType.value = "input";
-            elemType.id = "elemType" + n;
-            elemType.name = "elemType" + n;
+            elemType.id = "elemType" + n;        //
+            elemType.name = "elemType" + n;      //
 
             elem.type = "text";
-            elem.id = "element" + n;
-            elem.name = "element" + n;
+            elem.id = "element" + n;             // x
+            elem.name = "element" + n;           // x
             elem.required = true;
             elemDiv.appendChild(elem);
             elemDiv.appendChild(elemType);
 
+            deleteButtn.id = "delButt" + n;
+            deleteButtn.onclick = () => { deleteElement(elemDiv.id, n); };
+            deleteButtn.innerText = " Delete";
+            elemDiv.appendChild(deleteButtn);
+
             form.appendChild(elemDiv);
             elemNum.value++;
+        }
+
+        function deleteElement(divId, p){ // p := posizione elemento
+            const divToDelete = document.getElementById(divId);
+            let n = parseInt(elemNum.value);
+
+            divToDelete.remove();
+            if(p != n-1){
+                for(let i = p+1; i < n; i++){
+                    let elementDiv    = document.getElementById("elemDiv"  + i);
+                    let elemType      = document.getElementById("elemType" + i);
+                    let elementInput  = document.getElementById("element"  + i);
+                    let deleteButtn   = document.getElementById("delButt"  + i);
+
+                    elementDiv.id = "elemDiv" + (i - 1);
+
+                    elementType.id = "elemType" + (i - 1);
+                    elementType.name = "elemType" + (i - 1);
+
+                    elementInput.id = "element" + (i - 1);
+                    elementInput.name = "element" + (i - 1);
+
+                    deleteButtn.id = "delButt" + (i - 1);
+                    deleteButtn.onclick = () => { deleteElement(elementDiv.id, i-1); };
+                }
+            }
+            elemNum.value--;
         }
 
     </script>
