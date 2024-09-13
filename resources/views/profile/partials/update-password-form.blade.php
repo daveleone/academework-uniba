5e09e3e1b27e99da73925f340f8abb023a649e55
daveleone <it.davide.leone@gmail.com>
2024-09-13 21:29:56 +0200

    more updates

--- resources/views/profile/partials/update-password-form.blade.php
+++ resources/views/profile/partials/update-password-form.blade.php
@@ -1,13 +1,7 @@
 <section>
-    <header>
-        <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
-            @lang('trad.Update Password')
-        </h2>
-
-        <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
+    <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
         @lang('trad.Ensure your account is using a long, random password to stay secure.')
-        </p>
-    </header>
+    </p>
 
     <form method="post" action="{{ route('password.update') }}" class="mt-6 space-y-6">
         @csrf
@@ -32,7 +26,10 @@
         </div>
 
         <div class="flex items-center gap-4">
-            <x-primary-button>@lang('trad.Save')</x-primary-button>
+            <x-primary-button class="inline-flex items-center px-4 py-2 bg-indigo-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-indigo-700 active:bg-indigo-900 focus:outline-none focus:border-indigo-900 focus:ring ring-indigo-300 disabled:opacity-25 transition ease-in-out duration-300 hover:-translate-y-1">
+                <x-heroicon-s-key class="w-5 h-5 mr-2" />
+                @lang('trad.Save')
+            </x-primary-button>
 
             @if (session('status') === 'password-updated')
                 <p
