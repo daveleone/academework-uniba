e10b00a6a70c7417511cdf5dab0742a2877cfc64
Tato9602 <tatolevre@gmail.com>
2024-09-17 17:16:33 +0200

    cambiati colori

--- resources/views/forms/quiz/add_to_course.blade.php
+++ resources/views/forms/quiz/add_to_course.blade.php
@@ -18,36 +18,15 @@
 >
     <div class="relative max-h-full w-full max-w-md p-4">
         <!-- Modal content -->
-        <div class="relative rounded-lg bg-white shadow dark:bg-gray-700">
+        <div class="relative rounded-lg bg-white shadow dark:bg-gray-700 rounded-t-2xl rounded-b-2xl">
             <!-- Modal header -->
             <div
-                class="flex items-center justify-between rounded-t border-b p-4 dark:border-gray-600 md:p-5"
+                class="px-8 py-6 bg-indigo-600 rounded-t-2xl"
             >
-                <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
+                <h2 class="text-2xl font-bold text-white text-center">
                     @lang("trad.Add to course")
-                </h3>
-                <button
-                    type="button"
-                    class="ms-auto inline-flex h-8 w-8 items-center justify-center rounded-lg bg-transparent text-sm text-gray-400 hover:bg-gray-200 hover:text-gray-900 dark:hover:bg-gray-600 dark:hover:text-white"
-                    data-modal-toggle="AddToCourse-modal"
-                >
-                    <svg
-                        class="h-3 w-3"
-                        aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 14 14"
-                    >
-                        <path
-                            stroke="currentColor"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
-                            d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
-                        />
-                    </svg>
-                    <span class="sr-only">@lang("trad.Close modal")</span>
-                </button>
+                </h2>
+                    
             </div>
             <!-- Modal body -->
             <form
@@ -168,26 +147,21 @@
                         id="offset"
                         value="getOffset()"
                     />
-                    <button
-                        id="addToCourse-Submit"
-                        type="submit"
-                        class="inline-flex items-center rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                    >
-                        <svg
-                            class="-ms-1 me-1 h-5 w-5"
-                            fill="currentColor"
-                            viewBox="0 0 20 20"
-                            xmlns="http://www.w3.org/2000/svg"
-                        >
-                            <path
-                                fill-rule="evenodd"
-                                d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
-                                clip-rule="evenodd"
-                            ></path>
-                        </svg>
-                        @lang("trad.Add to course")
-                    </button>
-
+                    
+                        <button type="submit" class="group inline-flex justify-center py-3 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 transition duration-300 ease-in-out transform hover:-translate-y-1">
+                            <span class="absolute left-0 inset-y-0 flex items-center pl-3">
+                                <x-heroicon-s-plus-circle class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" />
+                            </span>
+                            @lang('trad.Add to course')
+                        </button>
+                        <a class="group flex justify-center py-3 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 transition duration-300 ease-in-out transform hover:-translate-y-1"
+                        data-modal-toggle="AddToCourse-modal">
+                            <span class="absolute left-0 inset-y-0 flex items-center pl-3">
+                                <x-heroicon-s-x-circle class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" />
+                            </span>
+                            @lang('trad.Cancel')
+                        </a>
+        
                     <script>
                         const submit =
                             document.getElementById('addToCourse-Submit');
