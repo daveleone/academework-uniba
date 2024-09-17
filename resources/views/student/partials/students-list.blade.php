a16c713fc7d94d30f78769bcda4f484bc5ab6c01
daveleone <it.davide.leone@gmail.com>
2024-09-17 21:43:42 +0200

    more tests

--- resources/views/student/partials/students-list.blade.php
+++ resources/views/student/partials/students-list.blade.php
@@ -6,7 +6,7 @@
                     <x-heroicon-s-user-circle class="w-8 h-8 text-gray-400" />
                 </div>
                 <div class="flex-1 min-w-0">
-                    <label for="student-{{ $student->id }}" class="text-sm font-medium text-gray-900 truncate">
+                    <label dusk="add-student-label" for="student-{{ $student->id }}" class="text-sm font-medium text-gray-900 truncate">
                         {{ $student->user->name }}
                     </label>
                     <p class="text-sm text-gray-500 truncate">
@@ -14,7 +14,7 @@
                     </p>
                 </div>
                 <div class="inline-flex items-center text-base font-semibold text-gray-900">
-                    <input id="student-{{ $student->id }}" name="students[]" type="checkbox" value="{{ $student->id }}" class="w-4 h-4 text-indigo-600 bg-gray-100 border-gray-300 rounded focus:ring-indigo-600 focus:ring-2">
+                    <input dusk="add-student-checkbox" id="student-{{ $student->id }}" name="students[]" type="checkbox" value="{{ $student->id }}" class="w-4 h-4 text-indigo-600 bg-gray-100 border-gray-300 rounded focus:ring-indigo-600 focus:ring-2">
                 </div>
             </div>
         </li>
