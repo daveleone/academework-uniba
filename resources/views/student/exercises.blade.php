a0462e0ba00191ea2875d57661ccb3a87410f01b
daveleone <it.davide.leone@gmail.com>
2024-09-17 23:16:41 +0200

    tests

--- resources/views/student/exercises.blade.php
+++ resources/views/student/exercises.blade.php
@@ -63,7 +63,7 @@
 
                                 @if(!$exam_taken[$quiz_id])
                                     <div class="mt-4">
-                                        <a href="{{route('student.exam', ['courses'=>$course->id, 'quiz'=>$quizData->id]) }}" class="inline-flex items-center px-4 py-2 bg-indigo-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 transition ease-in-out duration-150">
+                                        <a dusk="start-quiz" href="{{route('student.exam', ['courses'=>$course->id, 'quiz'=>$quizData->id]) }}" class="inline-flex items-center px-4 py-2 bg-indigo-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 transition ease-in-out duration-150">
                                             <x-heroicon-s-play class="w-5 h-5 mr-2" />
                                             @lang('trad.Start quiz')
                                         </a>
